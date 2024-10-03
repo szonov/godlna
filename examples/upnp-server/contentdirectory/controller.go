99c8c679e041070e64e7b27bc81f281e3fe4c413
Sergey Zonov <sergey@zonov.com>
2024-10-03 20:19:40 +0700

    generate contentdirectory service with handler generator

--- examples/upnp-server/contentdirectory/controller.go
+++ examples/upnp-server/contentdirectory/controller.go
@@ -60,14 +60,73 @@ func (ctl *ServiceController) Handle(w http.ResponseWriter, r *http.Request) boo
 	return false
 }
 
+func (ctl *ServiceController) GetSearchCapabilities(action *handler.Action) error {
+	//in := action.ArgIn.(*ArgInGetSearchCapabilities)
+	//out := action.ArgOut.(*ArgOutGetSearchCapabilities)
+	return nil
+}
+func (ctl *ServiceController) GetSortCapabilities(action *handler.Action) error {
+	//in := action.ArgIn.(*ArgInGetSortCapabilities)
+	//out := action.ArgOut.(*ArgOutGetSortCapabilities)
+	return nil
+}
+func (ctl *ServiceController) GetSystemUpdateID(action *handler.Action) error {
+	//in := action.ArgIn.(*ArgInGetSystemUpdateID)
+	//out := action.ArgOut.(*ArgOutGetSystemUpdateID)
+	return nil
+}
 func (ctl *ServiceController) Browse(action *handler.Action) error {
 	//in := action.ArgIn.(*ArgInBrowse)
-	out := action.ArgOut.(*ArgOutBrowse)
-
-	out.Result = "Result"
-	out.NumberReturned = 10
-	out.TotalMatches = 100
-	out.UpdateID = 3
-
+	//out := action.ArgOut.(*ArgOutBrowse)
+	return nil
+}
+func (ctl *ServiceController) Search(action *handler.Action) error {
+	//in := action.ArgIn.(*ArgInSearch)
+	//out := action.ArgOut.(*ArgOutSearch)
+	return nil
+}
+func (ctl *ServiceController) CreateObject(action *handler.Action) error {
+	//in := action.ArgIn.(*ArgInCreateObject)
+	//out := action.ArgOut.(*ArgOutCreateObject)
+	return nil
+}
+func (ctl *ServiceController) DestroyObject(action *handler.Action) error {
+	//in := action.ArgIn.(*ArgInDestroyObject)
+	//out := action.ArgOut.(*ArgOutDestroyObject)
+	return nil
+}
+func (ctl *ServiceController) UpdateObject(action *handler.Action) error {
+	//in := action.ArgIn.(*ArgInUpdateObject)
+	//out := action.ArgOut.(*ArgOutUpdateObject)
+	return nil
+}
+func (ctl *ServiceController) ImportResource(action *handler.Action) error {
+	//in := action.ArgIn.(*ArgInImportResource)
+	//out := action.ArgOut.(*ArgOutImportResource)
+	return nil
+}
+func (ctl *ServiceController) ExportResource(action *handler.Action) error {
+	//in := action.ArgIn.(*ArgInExportResource)
+	//out := action.ArgOut.(*ArgOutExportResource)
+	return nil
+}
+func (ctl *ServiceController) StopTransferResource(action *handler.Action) error {
+	//in := action.ArgIn.(*ArgInStopTransferResource)
+	//out := action.ArgOut.(*ArgOutStopTransferResource)
+	return nil
+}
+func (ctl *ServiceController) GetTransferProgress(action *handler.Action) error {
+	//in := action.ArgIn.(*ArgInGetTransferProgress)
+	//out := action.ArgOut.(*ArgOutGetTransferProgress)
+	return nil
+}
+func (ctl *ServiceController) DeleteResource(action *handler.Action) error {
+	//in := action.ArgIn.(*ArgInDeleteResource)
+	//out := action.ArgOut.(*ArgOutDeleteResource)
+	return nil
+}
+func (ctl *ServiceController) CreateReference(action *handler.Action) error {
+	//in := action.ArgIn.(*ArgInCreateReference)
+	//out := action.ArgOut.(*ArgOutCreateReference)
 	return nil
 }
